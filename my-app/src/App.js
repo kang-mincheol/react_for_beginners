5b4160718cd4855908777895535a2852960fd949
mincheol <aaa6315@gmail.com>
2023-01-11 19:27:56 +0900

    react styles 연습

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
 import Button from "./Button";
+import styles from "./App.module.css";
 
 function App() {
   return (
     <div>
-      <h1>hi</h1>
+      <h1 className={styles.title}>hi</h1>
       <Button text={"Continue"} />
     </div>
   );
