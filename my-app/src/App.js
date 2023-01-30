93a732e35fbb55027b4608a8cf99d886bb74739e
mincheol <aaa6315@gmail.com>
2023-01-30 18:43:29 +0900

    todo list add

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,27 +1,32 @@
-import { useState, useEffect } from "react";
-
-function Hello() {
-  function hiFn() {
-    console.log("create");
-    return byeFn;
-  }
-
-  function byeFn() {
-    console.log("bye");
-  }
-
-  useEffect(hiFn, []);
-  return <h1>Hello</h1>;
-}
+import { useState } from "react";
 
 function App() {
-  const [showing, setShowing] = useState(false);
-  const onClick = () => setShowing((prev) => !prev);
+  const [toDo, setToDo] = useState("");
+  const [toDos, setToDos] = useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") {
+      return;
+    }
+    console.log(toDo);
+    setToDo("");
+    setToDos((currentArray) => [toDo, ...currentArray]);
+  }
+  console.log(toDos);
 
   return (
     <div>
-      {showing ? <Hello /> : null}
-      <button onClick={onClick}>{showing ? "Hide" : "Show"}</button>
+      <h1>My To Dos ({toDos.length})</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={toDo}
+          type="text"
+          placeholder="Write your to do..."
+        />
+        <button>Add To Do</button>
+      </form>
     </div>
   );
 }
